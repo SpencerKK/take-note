56a0d35f8807b69dbb364ceb2ecd694f9439bb46
Spencer Kenealy <kenealys@me.com>
2020-07-12 10:52:49 -0700

    edit in server causing error in Heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/api/note', require('./routes/api/note'));
 app.use('/api/notebook', require('./routes/api/notebook'));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client.build'));
+  app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
